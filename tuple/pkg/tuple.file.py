e9f0ea268caa74b1aa617aeddb20e1f5e1603ed8
Snehalkhairnar9 <L00163434@student.lyit.ie>
2021-10-08 10:43:54 +0100

    First release of Hello World!\

---
+++ tuple/pkg/tuple.file.py
@@ -0,0 +1,16 @@
+
+
+empty_tuple = ()
+tuple_stud = ["L12345","L54321"]
+sub_name = ["Java_ooprogramming", "Python_Scripting"]
+dict_data = {"L12345": 40, "L54321": 70}
+
+print("name of the modules", sub_name)
+print("number and grades", dict_data)
+
+
+
+
+
+
+
